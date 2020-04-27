4ed1822944caea64b143e0d703926291f3671d4c
sylee <sylee8191@gmail.com>
2020-04-28 00:55:39 +0900

    changed Integer primary key to String primary key & done PostMapping to add DB

--- springboot-hibernate-test/src/main/java/com/test/springboothibernatetest/SpringbootHibernateTestApplication.java
+++ springboot-hibernate-test/src/main/java/com/test/springboothibernatetest/SpringbootHibernateTestApplication.java
@@ -1,5 +1,7 @@
 package com.test.springboothibernatetest;
 
+import com.test.springboothibernatetest.controller.ProductController;
+import com.test.springboothibernatetest.entity.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootHibernateTestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootHibernateTestApplication.class, args);
+        /*
+        Product product = new Product();
+        product.setName("a");
+        product.setId("b");
+        product.setQuantity(1);
+        product.setPrice(1.5);
+        ProductController productController = new ProductController();
+        productController.addProduct(product);
+        */
+
     }
 
 }
--- springboot-hibernate-test/src/main/java/com/test/springboothibernatetest/entity/Product.java
+++ springboot-hibernate-test/src/main/java/com/test/springboothibernatetest/entity/Product.java
@@ -4,6 +4,8 @@ package com.test.springboothibernatetest.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,11 +16,12 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "PRODUCT_TBL")
+@Table(name = "product_tbl")
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id;
     private String name;
     private int quantity;
